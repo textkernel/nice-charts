63ee9cd2b8f21de2375b2a84effd941ae759ace7
Carlo Bernardini <bernardini@textkernel.nl>
2017-11-21 11:17:22 +0100

    Updated storybook

--- storybook/stories/BarTable.js
+++ storybook/stories/BarTable.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, array } from '@storybook/addon-knobs';
+import { withKnobsOptions, object } from '@storybook/addon-knobs';
 import { BarTable } from 'nice-charts';
 
 const stories = storiesOf('BarTable', module);
 
-stories.addDecorator(withKnobs);
+stories.addDecorator(withKnobsOptions({
+    debounce: { wait: 500 },
+    timestamps: true
+}));
 
 const defaultValue = [["organization_name","count"],["Sandd",1815],["PostNL Holding B.V.",1578],["PostNL",1389],["Kruidvat",972],["Albert Heijn",738],["JobBoost",664],["Deloitte",595],["LIDL Nederland",495],["Rabobank",494],["Jumbo Supermarkten",476]];
 
 stories.add('Default', () => (
-    <BarTable data={ array('Data', defaultValue) } />
+    <BarTable data={ object('Data', defaultValue) } />
 ));
