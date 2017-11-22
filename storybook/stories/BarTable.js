f7d2624f753ba3517fc5c6632fa5e59c20b70fe2
Carlo Bernardini <bernardini@textkernel.nl>
2017-11-22 09:50:52 +0100

    BarTable support for URL or callback function on Click

--- storybook/stories/BarTable.js
+++ storybook/stories/BarTable.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobsOptions, object } from '@storybook/addon-knobs';
+import { withNotes } from '@storybook/addon-notes'
 import { BarTable } from 'nice-charts';
 
 const stories = storiesOf('BarTable', module);
@@ -10,9 +11,20 @@ stories.addDecorator(withKnobsOptions({
     timestamps: true
 }));
 
-const defaultValue = [["organization_name","count"],["Sandd",1815],["PostNL Holding B.V.",1578],["PostNL",1389],["Kruidvat",972],["Albert Heijn",738],["JobBoost",664],["Deloitte",595],["LIDL Nederland",495],["Rabobank",494],["Jumbo Supermarkten",476]];
+const defaultValue = [
+    ["organization_name","count"],
+    ["Sandd", 1815, 'https://www.sandd.nl/'],
+    ["PostNL Holding B.V.",1578],
+    ["PostNL",1389],
+    ["Kruidvat",972],
+    ["Albert Heijn",738],
+    ["JobBoost",664],
+    ["Deloitte",595],
+    ["LIDL Nederland",495],
+    ["Rabobank",494],
+    ["Jumbo Supermarkten",476]
+];
 
-stories.add('Default', () => (
-    <BarTable
-        data={ object('Data', defaultValue) } />
-));
+const notes = "You can optionally provide a URL or callback function for each entry in the list that will be executed on click";
+
+stories.add('Default', withNotes(notes)(() => <BarTable data={ object('Data', defaultValue) } />));
