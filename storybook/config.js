6887b74061601d7d4d651ddd8152d52f0409df34
Carlo Bernardini <bernardini@textkernel.nl>
2017-11-24 14:51:00 +0100

    LineChart: Support custom date / time format

--- storybook/config.js
+++ storybook/config.js
@@ -1,4 +1,9 @@
 import { configure } from '@storybook/react';
+import { setDefaults } from '@storybook/addon-info';
+
+setDefaults({
+    header: true
+});
 
 function loadStories() {
 	require('./stories/index.js');
